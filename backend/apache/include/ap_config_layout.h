1f369875f1d08c25f8d201b112bb9f1c57845555
Franklin Abodo <fabod001@fiu.edu>
2016-10-14 15:39:01 -0400

    added apache web server installation

---
+++ backend/apache/include/ap_config_layout.h
@@ -0,0 +1,64 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  ap_config_layout.h
+ * @brief Apache Config Layout
+ */
+
+#ifndef AP_CONFIG_LAYOUT_H
+#define AP_CONFIG_LAYOUT_H
+
+/* Configured Apache directory layout */
+#define DEFAULT_PREFIX "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache"
+#define DEFAULT_EXP_EXEC_PREFIX "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache"
+#define DEFAULT_REL_EXEC_PREFIX ""
+#define DEFAULT_EXP_BINDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/bin"
+#define DEFAULT_REL_BINDIR "bin"
+#define DEFAULT_EXP_SBINDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/bin"
+#define DEFAULT_REL_SBINDIR "bin"
+#define DEFAULT_EXP_LIBEXECDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/modules"
+#define DEFAULT_REL_LIBEXECDIR "modules"
+#define DEFAULT_EXP_MANDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/man"
+#define DEFAULT_REL_MANDIR "man"
+#define DEFAULT_EXP_SYSCONFDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/conf"
+#define DEFAULT_REL_SYSCONFDIR "conf"
+#define DEFAULT_EXP_DATADIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache"
+#define DEFAULT_REL_DATADIR ""
+#define DEFAULT_EXP_INSTALLBUILDDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/build"
+#define DEFAULT_REL_INSTALLBUILDDIR "build"
+#define DEFAULT_EXP_ERRORDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/error"
+#define DEFAULT_REL_ERRORDIR "error"
+#define DEFAULT_EXP_ICONSDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/icons"
+#define DEFAULT_REL_ICONSDIR "icons"
+#define DEFAULT_EXP_HTDOCSDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/htdocs"
+#define DEFAULT_REL_HTDOCSDIR "htdocs"
+#define DEFAULT_EXP_MANUALDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/manual"
+#define DEFAULT_REL_MANUALDIR "manual"
+#define DEFAULT_EXP_CGIDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/cgi-bin"
+#define DEFAULT_REL_CGIDIR "cgi-bin"
+#define DEFAULT_EXP_INCLUDEDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/include"
+#define DEFAULT_REL_INCLUDEDIR "include"
+#define DEFAULT_EXP_LOCALSTATEDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache"
+#define DEFAULT_REL_LOCALSTATEDIR ""
+#define DEFAULT_EXP_RUNTIMEDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/logs"
+#define DEFAULT_REL_RUNTIMEDIR "logs"
+#define DEFAULT_EXP_LOGFILEDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/logs"
+#define DEFAULT_REL_LOGFILEDIR "logs"
+#define DEFAULT_EXP_PROXYCACHEDIR "/home/fabodo/GitHub/Agricultural-Robotics-Ver-1.0/backend/apache/proxy"
+#define DEFAULT_REL_PROXYCACHEDIR "proxy"
+
+#endif /* AP_CONFIG_LAYOUT_H */
