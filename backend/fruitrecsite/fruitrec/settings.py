352468b0b2bfa926b8bd550477504e527fe34729
Franklin Abodo <fabod001@fiu.edu>
2016-12-09 10:50:55 -0500

    updated pgdb to reflect graphene-django to just graphene model changes.

--- backend/fruitrecsite/fruitrec/settings.py
+++ backend/fruitrecsite/fruitrec/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'analytics.apps.AnalyticsConfig',
+    'analytics.apps.AnalyticsConfig',
     'graphene_django',
-    'analytics',
+    # 'analytics',
 ]
 
 MIDDLEWARE = [
